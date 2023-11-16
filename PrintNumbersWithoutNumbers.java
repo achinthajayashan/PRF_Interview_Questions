45f14b04a5ba5f3da8dd720665b7323ffbe8fa57
achinthajayashan <achintha.uns.2018@gmail.com>
2023-11-16 22:20:18 +0530

    print numbers without numbers

--- PrintNumbersWithoutNumbers.java
+++ PrintNumbersWithoutNumbers.java
@@ -1,7 +1,7 @@
 public class PrintNumbersWithoutNumbers {
     public static void main(String[] args) {
         int one = 'A' / 'A'; // 1
-        int hundred = 'A' + '#'; // 100
+        int hundred = 'd'; // 100    one+'c'
 
         for (int i = one; i <= hundred; i++) {
             System.out.println(i);
